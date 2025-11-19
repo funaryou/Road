7c77a3a5ab5bf26eeb7f6e395fd14c92f8bbefab
Enzo <bannaienzo0517@gmail.com>
2025-11-19 17:11:49 +0900

    TopControllerのやつをやりました。ご確認をお願いします

---
+++ laravel_app/app/Http/Controllers/TopController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TopController extends Controller
+{
+    // トップページ
+    public function top()
+    {
+        //
+    }
+
+    // ツアー入力フォーム
+    public function tourForm()
+    {
+        //
+    }
+
+    // ツアー選択処理
+    public function tourSelect(Request $request)
+    {
+        //
+    }
+}
