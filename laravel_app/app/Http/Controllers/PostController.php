470f9b3c0ebca5b92da0586025a26171009729ae
yasui <tennta25kdgn@gmail.com>
2025-11-19 16:53:59 +0900

    PostControllerやりました

---
+++ laravel_app/app/Http/Controllers/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+            public function postFrom()
+    {
+        
+    }
+
+            public function postStore()
+    {
+        
+    }
+
+        public function posts()
+    {
+        
+    }
+}
