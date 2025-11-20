2b4fe9fa5210f2d1c90a1446bbd36d0b25b936f0
Enzo <bannaienzo0517@gmail.com>
2025-11-20 11:06:29 +0900

    web.phpの変更を戻した

--- laravel_app/routes/web.php
+++ laravel_app/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TopController;
 use App\Http\Controllers\TourController;
 use App\Http\Controllers\PostController;
@@ -41,3 +41,6 @@ Route::middleware('auth')->group(function () {
         Route::post('/', [PostController::class, 'postStore'])->name('post.store');
     });
 });
+
+
+
